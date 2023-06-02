c1c6082e29892bee440ff7f1700c43353e53cfbb
5afagy <115117722+5afagy@users.noreply.github.com>
2023-06-02 07:21:30 -0500

    Update index1.php

--- index.php
+++ index.php
@@ -1,13 +1,55 @@
 <?php
-include_once __DIR__ . '/../include/prepend.inc';
-$i = 0;
-do {
-    $y = date("Y") - $i;
-    if (file_exists("./$y.php")) {
-        mirror_redirect("/archive/$y.php");
-        break;
-    }
-} while (++$i < 3);
+//echo "<pre>";
+//print_r($_SERVER);
 
-include_once __DIR__ . '/../include/errors.inc';
-error_noservice();
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 align="center" >Welcome</h1>
+    <table class="table table-bordered">
+        <thead>
+
+
+            <tr>
+                <td>Name</td>
+                <td>Value</td>
+            </tr>
+            <tbody>
+                <?php foreach($_SERVER as $key => $value):?>
+                <tr>
+                    <td><?php echo $key; ?></td>
+                    <td><?php echo $value; ?></td>
+                </tr>
+                <tr>
+                        <!--Compine URL from super globel server-->
+                    <td><?php echo $_SERVER['REQUEST_SCHEME']."://".$_SERVER['REMOTE_ADDR'].$_SERVER['PHP_SELF']?></td>
+                    <td><?php 
+                            //$url = explode("/", $_SERVER['SCRIPT_NAME']);
+                            //$name = explode(".",count($url)-1);
+                            
+                    ?></td>
+                </tr>
+                <?php  endforeach;?>
+            </tbody>
+        </thead>
+    </table>
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
